06602e8c3bdccd5637642bba64ea2b701b85d836
listenGrey <tinghui561@outlook.com>
2024-01-31 19:17:41 +0800

    add queue

---
+++ queue/queue.go
@@ -0,0 +1,38 @@
+package queue
+
+type Queue struct {
+	items []interface{}
+}
+
+func New() *Queue {
+	return &Queue{}
+}
+
+func (q *Queue) Enqueue(item interface{}) {
+	q.items = append(q.items, item)
+}
+
+func (q *Queue) Dequeue() interface{} {
+	if len(q.items) == 0 {
+		return nil
+	}
+
+	item := q.items[0]
+	q.items = q.items[1:]
+	return item
+}
+
+func (q *Queue) Peek() interface{} {
+	if len(q.items) == 0 {
+		return nil
+	}
+	return q.items[0]
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.items) == 0
+}
+
+func (q *Queue) Size() int {
+	return len(q.items)
+}
